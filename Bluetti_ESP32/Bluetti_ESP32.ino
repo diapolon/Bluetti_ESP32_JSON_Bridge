f131f2712b7f92fa1c893e55b71331d6db71d399
pilip-d <philipdell@gmx.de>
2023-08-17 16:17:31 +0200

    removed display from code

--- Bluetti_ESP32/Bluetti_ESP32.ino
+++ Bluetti_ESP32/Bluetti_ESP32.ino
@@ -2,7 +2,6 @@
 #include "BTooth.h"
 #include "MQTT.h"
 #include "config.h"
-#include "display.h"
 
 unsigned long lastTime1 = 0;
 unsigned long timerDelay1 = 3000;
@@ -19,21 +18,14 @@ void setup() {
   #ifdef SLEEP_TIME_ON_BT_NOT_AVAIL
     esp_sleep_enable_timer_wakeup(SLEEP_TIME_ON_BT_NOT_AVAIL * 60 * 1000000ULL);
   #endif
-  #ifdef DISPLAYSSD1306
-    initDisplay();
-  #endif
+
   initBWifi(false);
   initBluetooth();
   initMQTT();
-  #ifdef DISPLAYSSD1306
-    wrDisp_Status("Running!");
-  #endif
+
 }
 
 void loop() {
-  #ifdef DISPLAYSSD1306
-    handleDisplay();
-  #endif
   handleBluetooth();
   handleMQTT(); 
   handleWebserver();
